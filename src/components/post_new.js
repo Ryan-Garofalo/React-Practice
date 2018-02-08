08184e634259a642ff5488fd5cd678c4ff415c93
Ryan Garofalo <ryangarofalo100@gmail.com>
2018-02-07 19:28:42 -0700

    fixed react router bug

---
+++ src/components/post_new.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class PostNew extends Component {
+
+	render() {
+		return (
+			<div>
+				<form>
+
+					<input typer="text"></input>
+					<input typer="text"></input>
+					<input typer="text"></input>
+
+
+
+				</form>
+
+
+
+			</div>
+		);
+	}
+
+}
+
+export default PostNew;
